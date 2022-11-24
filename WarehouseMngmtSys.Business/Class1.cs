c1a8fd74936d70def754f6e35af05d5a2147e246
Robert <rsmacias@gmail.com>
2022-11-24 12:34:29 -0500

    Adding Order Processor class.

--- WarehouseMngmtSys.Business/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace warehouseManagementSystem.Business;
-public class Class1
-{
-
-}
---
+++ WarehouseMngmtSys.Business/OrderProcessor.cs
@@ -0,0 +1,16 @@
+﻿using warehouseManagementSystem.Domain;
+
+namespace warehouseManagementSystem.Business;
+
+public class OrderProcessor {
+
+    private void Initialize (Order order) {
+        Console.WriteLine($"Initializing Order with Order number: {order.Id}");
+    }
+
+    public void Process (Order order) {
+        Initialize(order);
+        Console.WriteLine($"Finalizing Order Processing for Order number: {order.Id}");
+    }
+
+}
