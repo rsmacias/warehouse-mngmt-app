b71474efdf8ddb327e85ba92220ae2c36aa66f6f
Robert <rsmacias@gmail.com>
2022-11-25 10:51:42 -0500

    Adding functionality for adding new customers. Besides this, this is the first step to build a console interface for the final user.

--- WarehouseMngmtSys/Program.cs
+++ WarehouseMngmtSys/Program.cs
@@ -36,20 +36,78 @@ if (isWindowsPlatform) {
 
 using var context = new WarehouseContext(optionsBuilder.Options);
 
-foreach (var order in context.Orders
-                        .Include(order => order.Customer)
-                        .Include(order => order.ShippingProvider)
-                        .Include(order => order.LineItems)
-                        .ThenInclude(lineItem => lineItem.Item)) {
-    Console.WriteLine("------------------------------------------------");
-    Console.WriteLine($"Order Id: {order.Id}");
-    Console.WriteLine($"Customer: {order.Customer.Name}");
-    Console.WriteLine($"Shipping Provider: {order.ShippingProvider.Name}");
+var showAll = () => {
+    foreach (var order in context.Orders
+                            .Include(order => order.Customer)
+                            .Include(order => order.ShippingProvider)
+                            .Include(order => order.LineItems)
+                            .ThenInclude(lineItem => lineItem.Item)) {
+        Console.WriteLine("------------------------------------------------");
+        Console.WriteLine($"Order Id: {order.Id}");
+        Console.WriteLine($"Customer: {order.Customer.Name}");
+        Console.WriteLine($"Shipping Provider: {order.ShippingProvider.Name}");
 
-    foreach (var lineItem in order.LineItems) {
-        Console.WriteLine($"\tItem: {lineItem.Item.Name}");
-        Console.WriteLine($"\tPrice: {lineItem.Item.Price}");    
+        foreach (var lineItem in order.LineItems) {
+            Console.WriteLine($"\tItem: {lineItem.Item.Name}");
+            Console.WriteLine($"\tPrice: {lineItem.Item.Price}");    
+        }
     }
-}
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadLine();
+};
 
-Console.ReadLine();
+var newCustomer = () => {
+    Console.WriteLine("Enter Customer data...");
+
+    Console.Write($"Name: ");
+    var name = Console.ReadLine();
+    Console.Write($"Address: ");
+    var address = Console.ReadLine();
+    Console.Write($"Postal Code: ");
+    var zipCode = Console.ReadLine();
+    Console.Write($"Country: ");
+    var country = Console.ReadLine();
+    Console.Write($"Phone Number: ");
+    var phoneNumber = Console.ReadLine();
+
+    var newCustomer = new Customer {
+        Name = name,
+        Address = address,
+        PostalCode = zipCode,
+        Country = country,
+        PhoneNumber = phoneNumber
+    };
+
+    context.Customers.Add(newCustomer);
+    context.SaveChanges();
+
+    Console.WriteLine("New customer added.\r\nPress any key to continue...");
+    Console.ReadLine();
+};
+
+var showMenu = () => {
+    Console.WriteLine("------------------------------------------------");
+    Console.WriteLine("0.- Exit");
+    Console.WriteLine("1.- Select All");
+    Console.WriteLine("2.- New Customer");
+};
+
+var gotToOption = (string option) => {
+    switch(option) {
+        case "1" : showAll(); break;
+        case "2" : newCustomer(); break;
+        default: break;
+    }
+};
+
+string option = string.Empty;
+do {
+    option = string.Empty;
+    Console.Clear();
+    showMenu();
+    Console.Write("Select an option: ");
+    option = Console.ReadLine();
+    gotToOption(option);
+} while(option != "0");
+
+Console.WriteLine("Bye!");
