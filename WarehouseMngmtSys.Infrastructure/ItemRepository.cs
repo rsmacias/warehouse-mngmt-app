41edd18dbef5ef4e3f689b9a68c809287815f0dc
Robert <rsmacias@gmail.com>
2022-12-04 16:06:32 -0500

    Adding ItemRepository class.

---
+++ WarehouseMngmtSys.Infrastructure/ItemRepository.cs
@@ -0,0 +1,18 @@
+namespace warehouseManagementSystem.Infrastructure;
+
+public class ItemRepository : GenericRepository<Item> {
+
+    public ItemRepository(WarehouseContext context) 
+        : base(context) {
+        
+    }
+
+    public override Item Update(Item entity) {
+        Item toUpdate = Get(entity.Id);
+
+        toUpdate.Price = entity.Price;
+        toUpdate.Name = entity.Name;
+
+        return base.Update(toUpdate);
+    }
+}
