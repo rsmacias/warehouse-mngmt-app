1f86a1bf1a34c5cc8e3bd569f6a676f6881c7505
Robert <rsmacias@gmail.com>
2022-12-04 17:17:01 -0500

    Decoupling Web Project: > Implementing Customer Repository class into Customer Controller class. > Removing entities and context from Data directory because they are not going to be used anymore. > Replacing the entities dependencies on View files to the new entities inside Infrastructure project.

--- WarehouseMngmtSys.Web/Controllers/CustomerController.cs
+++ WarehouseMngmtSys.Web/Controllers/CustomerController.cs
@@ -1,22 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
-using warehouseManagementSystem.Web.Data;
+using warehouseManagementSystem.Infrastructure;
 
 namespace warehouseManagementSystem.Web.Controllers;
 
 public class CustomerController : Controller {
-    private readonly WarehouseContext context = null;
 
-    public CustomerController(WarehouseContext context) {
-        this.context = context;
+    private readonly IRepository<Customer> _customerRepository;
+
+    public CustomerController(IRepository<Customer> customerRepository) {
+        _customerRepository = customerRepository;
     }
 
     public IActionResult Index(Guid? id) {
         if (id == null) {
-            var customers = context.Customers.ToList();
+            var customers = _customerRepository.All();
 
             return View(customers);
         } else {
-            var customer = context.Customers.Find(id.Value);
+            var customer = _customerRepository.Get(id.Value);
 
             return View(new[] { customer });
         }
