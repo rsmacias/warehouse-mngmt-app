275e36056932c1b42d323cfe9c233aeeb65e3da4
Robert <rsmacias@gmail.com>
2022-12-11 14:01:54 -0500

    feat: Adding Lazy Initialization for Logo field. OJO: When there is a backing field in the entity, EF doesn't load the data for that field at the first time (eager loading). So, I can implement lazy loading for this field. This can be witnessed by the Logo field.

--- WarehouseMngmtSys.Infrastructure/Data/Customer.cs
+++ WarehouseMngmtSys.Infrastructure/Data/Customer.cs
@@ -16,9 +16,14 @@ namespace warehouseManagementSystem.Infrastructure
         public string PostalCode { get; set; } = null!;
         public string Country { get; set; } = null!;
         public string PhoneNumber { get; set; } = null!;
-        private byte[]? logo;
+        private byte[]? logo;  // backing field
         public byte[]? Logo {
-            get => logo; 
+            get {  // Lazy Initialization
+                if (logo is null) {
+                    logo = LogoService.GetFor(Name);
+                }
+                return logo;
+            } 
             set => logo = value; 
         }
 
